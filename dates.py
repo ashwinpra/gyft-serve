53cc385fc0aeb82f5b09411ad45bb4399a6b6399
Sahil Shubham <sahil.shubham2000@gmail.com>
2022-08-03 22:13:11 +0530

    updated dates for autumn semester 2022-23 (#12)

--- dates.py
+++ dates.py
@@ -6,13 +6,13 @@ import pytz
 
 # SEM_BEGIN=datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
 # SEM_BEGIN=datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
-SEM_BEGIN = build_event.generateIndiaTime(2022, 1, 4, 0, 0)
+SEM_BEGIN = build_event.generateIndiaTime(2022, 8, 2, 0, 0)
 
-MID_TERM_BEGIN = build_event.generateIndiaTime(2022, 2, 21, 0, 0)
+MID_TERM_BEGIN = build_event.generateIndiaTime(2022, 9, 20, 0, 0)
 
-MID_TERM_END = build_event.generateIndiaTime(2022, 2, 25, 23, 59)
+MID_TERM_END = build_event.generateIndiaTime(2022, 9, 30, 23, 59)
 
-END_TERM_BEGIN = build_event.generateIndiaTime(2022, 4, 13, 0, 0)
+END_TERM_BEGIN = build_event.generateIndiaTime(2022, 11, 16, 0, 0)
 
 ## Sanity check
 
